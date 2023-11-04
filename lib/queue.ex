b925e085453b13b4651a4107db8cee7ee426d688
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-11-04 12:19:33 -0300

    chore: add dequeue function

--- lib/queue.ex
+++ lib/queue.ex
@@ -1,23 +1,59 @@
 defmodule Queue do
   use GenServer
 
+  require Logger
+
   @impl true
   def init(state) do
+    Logger.info("Init Queue")
+
     {:ok, state}
   end
 
   def start_link(_initial_state) do
-    GenServer.start_link(__MODULE__, %{})
+    GenServer.start_link(__MODULE__, [])
   end
 
   def enqueue(pid, element) do
     GenServer.cast(pid, {:enqueue, element})
   end
 
+  def dequeue(pid) do
+    GenServer.call(pid, :dequeue)
+  end
+
   @impl true
   def handle_cast({:enqueue, element}, state) do
-    new_state = [state | element]
+    new_state = List.insert_at(state, -1, element)
+
+    Logger.info(%{
+      element: element,
+      new_state: new_state,
+      message: "handle_cast :enqueue"
+    })
 
     {:noreply, new_state}
+  end
+
+  @impl true
+  def handle_call(:dequeue, _from, [head | tail]) do
+    Logger.info(%{
+      message: "handle_call :dequeue",
+      new_state: tail,
+      removed_element: head
+    })
+
+    {:reply, head, tail}
+  end
+
+  @impl true
+  def handle_call(:dequeue, _from, state) do
+    Logger.info(%{
+      message: "handle_call :dequeue",
+      new_state: state,
+      removed_element: nil
+    })
+
+    {:reply, nil, state}
   end
 end
--- test/queue_test.exs
+++ test/queue_test.exs
@@ -32,4 +32,30 @@ defmodule QueueTest do
       assert expected_response == response
     end
   end
+
+  describe "dequeue/2" do
+    test "when the state is not empty" do
+      {:ok, pid} = Queue.start_link(%{})
+
+      Queue.enqueue(pid, 1)
+      Queue.enqueue(pid, 2)
+      Queue.enqueue(pid, 3)
+
+      response = Queue.dequeue(pid)
+
+      expected_response = 1
+
+      assert expected_response == response
+    end
+
+    test "when the state is empty" do
+      {:ok, pid} = Queue.start_link(%{})
+
+      response = Queue.dequeue(pid)
+
+      expected_response = nil
+
+      assert expected_response == response
+    end
+  end
 end
